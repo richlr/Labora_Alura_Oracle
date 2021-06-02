8e24caac7c1b61b667454598b4494ec6b53db31e
richlr <64228769+richlr@users.noreply.github.com>
2021-06-02 17:03:16 -0300

    Update buscar-pacientes.js
    Removi os comentários que eu havia deixado abaixo do código para diminuir o tamanho do arquivo.

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -24,23 +24,4 @@ xhr.addEventListener("load", function() {
 
 xhr.send();
 });
-/*
-botaoAdicionar.addEventListener("click", function(){
-    console.log("Buscando pacientes...");
 
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-
-    xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
-
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
-    });
-
-    xhr.send();
-});*/
